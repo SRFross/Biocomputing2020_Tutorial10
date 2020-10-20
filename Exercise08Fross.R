fefb43e28c73f7d979efcbcb5bb76719376a35e1
Shaneann Fross <sfross@nd.edu>
2020-10-19 20:02:58 -0400

    First try for commiting this Exercise

---
+++ Exercise08Fross.R
@@ -0,0 +1,14 @@
+##Shaneann Fross
+##Exercise 08 Biocomputing
+
+setwd("/Users/Shaneann/Desktop/data-shell/Biocomputing2020_Tutorial10")
+
+##1. Create a graph with cummulative scores
+
+#read in data frame
+game <- read.delim(file="UWvMSU_1-22-13.txt", header=TRUE, stringsAsFactors=FALSE) 
+#Create a cummulative scores matrix
+
+#Plot the data
+
+
